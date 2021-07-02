bd12bb73067bf8d8e1cbff59a23561dbc75568f8
Motoka <motoka64@gmail.com>
2021-07-02 21:00:54 +0900

    サーバーの変更

--- src/composables/CycleRoute.ts
+++ src/composables/CycleRoute.ts
@@ -11,7 +11,7 @@ export interface CycleRoute {
 function fetchCycleRoute(lls: LngLat[], callback: (route: CycleRoute | null) => void): void {
     if (lls.length < 2) callback(null);
     const client = axios.create({
-        baseURL: "https://cycle-route.tk/route/",
+        baseURL: "https://cyc-route.com/route/",
     });
     const query = lls.map((ll) => `${ll.lng.toFixed(5)},${ll.lat.toFixed(5)}`).join(";")
     const fetchRoute = (): AxiosPromise<CycleRoute> => client.get(query, {
